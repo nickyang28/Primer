c58ede2092ade23ab1427fc6e8a5db78cb31b6a9
Nick Yang <yangfeichi@163.com>
2020-07-13 00:49:25 +0800

    Update on 2020.07.13

--- Sales_data.h
+++ Sales_data.h
@@ -5,9 +5,22 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 struct Sales_data {
+    std::string isbn() const {return this->bookNo;}
+    Sales_data& combine(const Sales_data&);
+    double avg_price() const;
     std::string bookNo;
     int units_sold = 0;
     double price = 0.0;
     double revenue = 0.0;
 };
+
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+double Sales_data::avg_price() const
+{
+
+}
+
 #endif
