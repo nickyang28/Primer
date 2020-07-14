ec508fc54921f3220c9c25de7dcec2d3e07d4e9c
Doveyang <yangfeichi@163.com>
2020-07-14 22:32:00 +0800

    Update on 2020.07.14

--- 7.3.1.cpp
+++ 7.3.1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 class Screen{
+friend class Window_mgr;
 public:
     typedef std::string::size_type pos;
     Screen() = default;
@@ -23,7 +24,7 @@ private:
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
-    void do_display(std::ostream &os) const {os<< contents;}
+    void do_display(std::ostream &os) const {os << contents;}
 };
 
 inline
@@ -48,9 +49,18 @@ inline Screen &Screen::set(pos r, pos col, char ch)
 }
 
 class Window_mgr{
+public:
+    typedef std::vector<Screen>::size_type ScreenIndex;
+    void clear(ScreenIndex);
 private:
     std::vector<Screen> screens{Screen(24, 80, ' ')};
 };
+
+void Window_mgr::clear(ScreenIndex i)
+{
+    Screen &s = screens[i];
+    s.contents = string(s.height * s.width, ' ');
+}
 
 int main()
 {
