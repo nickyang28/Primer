739d37aa2652e11608c5a25006d04e6bea8b82de
Doveyang <yangfeichi@163.com>
2020-07-21 20:02:57 +0800

    Update on 2020.07.21

--- 8.3.1.cpp
+++ 8.3.1.cpp
@@ -2,6 +2,7 @@
 // Created by Feichi Yang on 2020/7/17.
 //
 #include <fstream>
+#include <array>
 #include <string>
 #include <vector>
 #include <ctime>
@@ -16,24 +17,33 @@ int main()
 {
     clock_t t1, t2;
     t1 = clock();
+
     ifstream click_log(R"(D:\PyProjects\TencentAdv\data\w2v_click_creative_200d.txt)", ifstream::in);
+    if(!click_log)
+        return -1;
+
     string line;
-    vector<string> lines;
-    unordered_map<string, vector<double>> dict;
+    string key;
+    double weight;
+    unsigned long rows, dim;
     getline(click_log, line);
+    istringstream info(line);
+    info >> rows >> dim;
+
+    unordered_map<string, array<double, 200>> dict;
+    dict.reserve(rows);
+    int count = 0;
     while (getline(click_log, line)){
         istringstream record(line);
-        string key;
-        vector<double> weights;
-        double weight;
+        int i = 0;
         record >> key;
         while (record >> weight){
-            weights.push_back(weight);
+            dict[key].at(i++) = weight;
         }
-        dict[key] = weights;
+        ++count;
     }
     t2 = clock();
     cout << "Reading time: "  << double(t2 - t1) / CLOCKS_PER_SEC << "s" << endl;
-    //cout << dict["108133"] << endl;
+    cout << dict["108133"].at(0) << endl;
     return 0;
 }
